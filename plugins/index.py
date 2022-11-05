bdbc395f266615b964828093aab1e54b78567efd
tamilthalaiva <110980013+tamilthalaiva@users.noreply.github.com>
2022-11-05 22:57:02 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -22,7 +22,7 @@ async def index_files(bot, query):
     if raju == 'reject':
         await query.message.delete()
         await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been decliened by our moderators.\nContact Support @TechnoMindzChat.',
+                               f'Your Submission for indexing {chat} has been decliened by our moderators.\nContact Support @tamilthalaiva4.',
                                reply_to_message_id=int(lst_msg_id))
         return
 
@@ -116,7 +116,7 @@ async def send_for_index(bot, message):
     await bot.send_message(LOG_CHANNEL,
                            f'#IndexRequest\n\nBy : {message.from_user.mention} (<code>{message.from_user.id}</code>)\nChat ID/ Username - <code> {chat_id}</code>\nLast Message ID - <code>{last_msg_id}</code>\nInviteLink - {link}',
                            reply_markup=reply_markup)
-    await message.reply('ThankYou For the Contribution😀, Wait For My Moderators to verify✅ the files.\nMade By @TechnoMindzChat')
+    await message.reply('ThankYou For the Contribution😀, Wait For My Moderators to verify✅ the files.\nMade By @Tamilthalaiva3)
 
 
 @Client.on_message(filters.command('setskip') & filters.user(ADMINS))
